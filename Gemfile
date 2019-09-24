a19141e657295d3a294140e17bb20f631d42ab32
Sergey Kraykin <kraykin@gmail.com>
2019-09-24 19:15:24 +0000

    Add faker gem and db seed

--- Gemfile
+++ Gemfile
@@ -3,9 +3,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.4'
 
-gem 'will_paginate', '3.1.8'
-gem 'friendly_id',   '5.2.5'
 gem 'devise',        '4.7.1'
+gem 'friendly_id',   '5.2.5'
+gem 'will_paginate', '3.1.8'
+gem 'faker',         '2.4.0'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Users
+User.create!(username:              "adm",
+             email:                 "admin@example.com",
+             password:              "1q@W3e$R",
+             password_confirmation: "1q@W3e$R")
+
+39.times do |n|
+  username  = Faker::Superhero.name
+  email = "user-#{n+1}@example.com"
+  password = "1qazXSW@"
+  User.create!(username:              username,
+               email:                 email,
+               password:              password,
+               password_confirmation: password)
+end
+
+# Posts
+users = User.take(10)
+40.times do
+  content = Faker::Lorem.paragraph(sentence_count: rand(5..20))
+  users.sample.posts.create!(content: content)
+end
