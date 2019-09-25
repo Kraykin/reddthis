57e49fbef51a6217657b98eefed71a4864f57493
Sergey Kraykin <kraykin@gmail.com>
2019-09-25 09:52:30 +0000

    Change showing of users index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.paginate(page: params[:page], per_page: 10)
+    @users = User.paginate(page: params[:page])
   end
 
   def show
