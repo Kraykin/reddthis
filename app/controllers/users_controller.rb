f14a20dc6210dea7df5d2198e9671dbfebd037ba
Sergey Kraykin <kraykin@gmail.com>
2019-09-28 21:04:26 +0300

    Revert "Refactor controllers"
    This reverts commit c81447f608108b3381e19056f1902a7a9228a352.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, except: [:show]
+  before_action :set_user, only: [:show]
   load_and_authorize_resource
 
   def index
@@ -8,5 +9,11 @@ class UsersController < ApplicationController
 
   def show
     @posts = @user.posts.paginate(page: params[:page], per_page: 10)
+  end
+
+  private
+
+  def set_user
+    @user = User.friendly.find(params[:id])
   end
 end
