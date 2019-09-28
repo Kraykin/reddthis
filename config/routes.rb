941e554f8508b40e022524ca9116324214580d10
Sergey Kraykin <kraykin@gmail.com>
2019-09-28 11:41:24 +0000

    Fix soft delete users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'posts#index'
   resources :posts
-  devise_for :users, :controllers => { :registrations => 'registrations' }
+  devise_for :users, :controllers => { :registrations => 'users/registrations' }
   resources :users, only: [:index, :show]
 end
