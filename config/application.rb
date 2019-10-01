4ff5b5fb8f9092e77ff0343baa935b55ee7c79b5
Sergey Kraykin <kraykin@gmail.com>
2019-10-01 16:38:27 +0000

    Fix rspec

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,5 @@ module Reddthis
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    config.web_console.whitelisted_ips = '10.0.2.2'
   end
 end
