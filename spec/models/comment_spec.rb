04c6d88f9d4ebff9cfac1b2453e607a01f9cdf00
Sergey Kraykin <kraykin@gmail.com>
2019-10-06 16:20:55 +0300

    Fix tests of models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,6 +7,6 @@ RSpec.describe Comment, type: :model do
     it { should belong_to :user }
     it { should belong_to :post }
     it { should validate_presence_of(:content) }
-    it { expect(post.comments.first.content).to eq('Comment body 1') }
+    it { expect(post.comments.first.content).to match(/Comment body/) }
   end
 end
