550f3c7955085aee0a37a26c8c17b39d3ca9b5e6
Sergey Kraykin <kraykin@gmail.com>
2019-10-11 22:03:37 +0300

    Add test for user interacting with posts

---
+++ spec/features/user_interacts_with_posts_spec.rb
@@ -0,0 +1,97 @@
+require 'rails_helper'
+
+feature 'User interacts with posts' do
+  background do
+    create(:post, content: 'Own post body')
+    create(:post, content: 'Foreign post body')
+    sign_in User.first
+  end
+
+  feature 'visit post' do
+    given(:post_text) do
+      page.find(:xpath, "//div[@*='mx-2 text-justify' and not(*)]")
+    end
+
+    scenario 'own from main page' do
+      visit root_path
+      click_link 'Own post body'
+      expect(post_text).to have_text 'Own post body'
+    end
+
+    scenario 'own from own profile' do
+      visit '/users/1'
+      click_link 'Own post body'
+      expect(post_text).to have_text 'Own post body'
+    end
+
+    scenario 'foreign from main page' do
+      visit root_path
+      click_link 'Foreign post body'
+      expect(post_text).to have_text 'Foreign post body'
+    end
+
+    scenario 'foreign from foreign profile' do
+      visit '/users/2'
+      click_link 'Foreign post body'
+      expect(post_text).to have_text 'Foreign post body'
+    end
+  end
+
+  given(:rating_xpath) { "//*[@*='col-1' and */*[contains(@href, '/1/up')]]/span" }
+
+  feature 'positively rate own post', js: true do
+    scenario 'from post page' do
+      visit '/posts/1'
+      click_link '+'
+      expect(page).to have_text '1'
+    end
+
+    scenario 'from own profile' do
+      visit '/users/1'
+      find(:xpath, "//*[contains(@href, '/posts/1/upvote')]").click
+      expect(find(:xpath, rating_xpath).text).to have_text '1'
+    end
+  end
+
+  feature 'positively rate foreign post', js: true do
+    scenario 'from post page' do
+      visit '/posts/2'
+      click_link '+'
+      expect(page).to have_text '1'
+    end
+
+    scenario 'from foreign profile' do
+      visit '/users/2'
+      find(:xpath, "//*[contains(@href, '/posts/2/upvote')]").click
+      expect(find(:xpath, rating_xpath).text).to have_text '1'
+    end
+  end
+
+  feature 'negatively rate own post', js: true do
+    scenario 'from post page' do
+      visit '/posts/1'
+      click_link '-'
+      expect(page).to have_text '-1'
+    end
+
+    scenario 'from own profile' do
+      visit '/users/1'
+      find(:xpath, "//*[contains(@href, '/posts/1/downvote')]").click
+      expect(find(:xpath, rating_xpath).text).to have_text '-1'
+    end
+  end
+
+  feature 'negatively rate foreign post', js: true do
+    scenario 'from post page' do
+      visit '/posts/2'
+      click_link '-'
+      expect(page).to have_text '-1'
+    end
+
+    scenario 'from foreign profile' do
+      visit '/users/2'
+      find(:xpath, "//*[contains(@href, '/posts/2/downvote')]").click
+      expect(find(:xpath, rating_xpath).text).to have_text '-1'
+    end
+  end
+end
